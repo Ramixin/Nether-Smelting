ed33b8658b2cf87dfe33e1dc04e7cfc69a1c24d2
Ram <104311709+RamGemes@users.noreply.github.com>
2025-01-18 11:02:56 -0500

    V:0.0.3 - Fixed improper method mixin signature

---
+++ README.md
@@ -0,0 +1,22 @@
+<div style="text-align: center;">
+<img src="brand.png"  alt="Nether Smelting Logo">
+<h2>
+<b>A simple mod that makes furnaces smelt twice as fast in the Nether</b>
+</h2>
+<br>
+<a href="">
+<img alt="modrinth" height="56" src="https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/cozy/available/modrinth_vector.svg">
+</a>
+<a href="https://github.com/Ramixin/Nether-Smelting">
+<img alt="github" height="56" src="https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/cozy/available/github_vector.svg">
+</a>
+<br>
+<img alt="fabric" height="40" src="https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/compact/supported/fabric_vector.svg">
+<img alt="no forge" height="40" src="https://cdn.jsdelivr.net/npm/@intergrav/devins-badges@3/assets/compact/unsupported/forge_vector.svg">
+
+</div>
+
+## License
+
+---
+This project is under an MIT license
--- src/main/java/net/ramixin/nethersmelting/mixins/AbstractFurnaceBlockEntityMixin.java
+++ src/main/java/net/ramixin/nethersmelting/mixins/AbstractFurnaceBlockEntityMixin.java
@@ -1,7 +1,6 @@
 package net.ramixin.nethersmelting.mixins;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.entity.AbstractFurnaceBlockEntity;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.world.ServerWorld;
@@ -15,10 +14,10 @@ import java.util.Optional;
 @Mixin(AbstractFurnaceBlockEntity.class)
 public class AbstractFurnaceBlockEntityMixin {
 
-    @ModifyReturnValue(method = "getCookTime", at = @At("RETURN"))
-    private static int divideCookTimeIfInNether(int original, @Local(argsOnly = true) ServerWorld world, @Local(argsOnly = true) AbstractFurnaceBlockEntity entity) {
-        if(entity.getWorld() == null) return original;
-        Optional<RegistryKey<DimensionType>> optionalKey = entity.getWorld().getDimensionEntry().getKey();
+    @ModifyReturnValue(method = "getCookTime(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/block/entity/AbstractFurnaceBlockEntity;)I", at = @At("RETURN"))
+    private static int divideCookTimeIfInNether(int original, ServerWorld world, AbstractFurnaceBlockEntity entity) {
+        if(world == null) return original;
+        Optional<RegistryKey<DimensionType>> optionalKey = world.getDimensionEntry().getKey();
         if(optionalKey.isEmpty()) return original;
         if(optionalKey.get() == DimensionTypes.THE_NETHER) return original / 2;
         return original;
